96bea608176f1d1c93ce9c7830c946d3994d092c
Mark Kostick <markk@yesware.com>
2020-05-11 08:51:53 -0400

    Remove `wrap_exception`.  Referencing Exception#cause is automatic and more useful.

--- lib/yeti_exception/helpers.rb
+++ lib/yeti_exception/helpers.rb
@@ -1,17 +1,10 @@
 module YetiException
 
   # Mixin for constructing and raising YetiException::Error or its subclasses.
-  # Two convenience methods are provided, both on the class and on class
-  # instances:
-  #
-  # - raise_exception: Infers the class in which the exception is raised so that
-  #   it can be used in constructing the exception.
-  #
-  # - wrap_exception: Creates a new YetiException exception from an arbitrary
-  #   exception, maintaining the orignal's backtrace.  This is useful when
-  #   dealing with exceptions from third-party code, so that the extra features
-  #   of YetiException may be used without losing the location of the original
-  #   error.
+  # It provides the 'raise_exception' method, which infers the class in which
+  # the exception is raised so that it can be used in constructing the
+  # exception.  This method is provided both on the class and on class
+  # instances.
   #
   # Example usage:
   #
@@ -27,16 +20,6 @@ module YetiException
   #                           })
   #         end
   #       end
-  #
-  #       def bar(y)
-  #         ThirdParty.Bar(y)
-  #       rescue => ex
-  #         wrap_exception(ex,
-  #                        YetiException::Error,
-  #                        {
-  #                          y_value: y
-  #                        })
-  #       end
   #     end
 
   module Helpers
@@ -51,23 +34,6 @@ module YetiException
       raise exception_klass.new(self.class, details, *args)
     end
 
-    # Wrap an arbitrary exception in a new enhanced exception and reraise,
-    # preserving the original backtrace and merging the original message into
-    # the details.
-    #
-    # @param original_exception [Exception]
-    # @param new_exception_klass [Class]
-    # @param details [Hash]
-    # @param args [Object ...]
-    # @raise [YetiException::Error]
-    def wrap_exception(original_exception, new_exception_klass, details, *args)
-      new_details = details.merge(msg: original_exception.message)
-      new_exception = new_exception_klass.new(self.class, new_details, *args).tap do |ex|
-        ex.set_backtrace(original_exception.backtrace)
-      end
-      raise new_exception
-    end
-
     # Define the class methods.
     # yardoc doesn't provide a way to ignore methods, so...
 
@@ -75,14 +41,6 @@ module YetiException
     def self.included(klass)
       klass.define_singleton_method(:raise_exception) do |exception_klass, details, *args|
         raise exception_klass.new(klass, details, *args)
-      end
-
-      klass.define_singleton_method(:wrap_exception) do |original_exception, new_exception_klass, details, *args|
-        new_details = details.merge(msg: original_exception.message)
-        new_exception = new_exception_klass.new(klass, new_details, *args).tap do |ex|
-          ex.set_backtrace(original_exception.backtrace)
-        end
-        raise new_exception
       end
     end
 
@@ -94,18 +52,6 @@ module YetiException
     # Raise the specified exception, inferring the calling class automatically.
     #
     # @param exception_klass [Class]
-    # @param details [Hash]
-    # @param args [Object ...]
-    # @raise [YetiException::Error]
-
-    # @!method wrap_exception(original_exception, new_exception_klass, details, *args)
-    # @!scope class
-    # Wrap an arbitrary exception in a new enhanced exception and reraise,
-    # preserving the original backtrace and merging the original message into
-    # the details.
-    #
-    # @param original_exception [Exception]
-    # @param new_exception_klass [Class]
     # @param details [Hash]
     # @param args [Object ...]
     # @raise [YetiException::Error]
--- spec/yeti_exception/helpers_spec.rb
+++ spec/yeti_exception/helpers_spec.rb
@@ -46,47 +46,6 @@ describe YetiException::Helpers do
     end
   end
 
-  describe '#wrap_exception' do
-    let(:klass) do
-      Class.new do
-        include YetiException::Helpers
-
-        def oops
-          # Will be stubbed to raise.  We can't just raise inline because we
-          # want to reference the exception's backtrace.
-        end
-
-        def boom
-          oops
-        rescue => ex
-          wrap_exception(ex,
-                         YetiException::Error,
-                         { some: 'details' })
-        end
-      end
-    end
-    let(:oops) { StandardError.new('OOPS!') }
-    let(:instance) { klass.new }
-
-    before(:each) do
-      allow(instance).to receive(:oops).and_raise(oops)
-    end
-
-    subject { instance.boom }
-
-    it 'raises a wrapped exception' do
-      expect { subject }.to raise_error do |error|
-        expect(error).to be_a(YetiException::Error)
-        expect(error.klass).to eq(klass)
-        expect(error.details).to eq({
-                                      msg: 'OOPS!',
-                                      some: 'details'
-                                    })
-        expect(error.backtrace).to eq(oops.backtrace)
-      end
-    end
-  end
-
   describe '.raise_exception' do
     let(:klass) do
       Class.new do
@@ -130,46 +89,6 @@ describe YetiException::Helpers do
           expect(error.transient).to eq(true)
           expect(error.http_status).to eq(500)
         end
-      end
-    end
-  end
-
-  describe '.wrap_exception' do
-    let(:klass) do
-      Class.new do
-        include YetiException::Helpers
-
-        def self.oops
-          # Will be stubbed to raise.  We can't just raise inline because we
-          # want to reference the exception's backtrace.
-        end
-
-        def self.boom
-          oops
-        rescue => ex
-          wrap_exception(ex,
-                         YetiException::Error,
-                         { some: 'details' })
-        end
-      end
-    end
-    let(:oops) { StandardError.new('OOPS!') }
-
-    before(:each) do
-      allow(klass).to receive(:oops).and_raise(oops)
-    end
-
-    subject { klass.boom }
-
-    it 'raises a wrapped exception' do
-      expect { subject }.to raise_error do |error|
-        expect(error).to be_a(YetiException::Error)
-        expect(error.klass).to eq(klass)
-        expect(error.details).to eq({
-                                      msg: 'OOPS!',
-                                      some: 'details'
-                                    })
-        expect(error.backtrace).to eq(oops.backtrace)
       end
     end
   end
